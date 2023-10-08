33afe8f540ee6a392c695cc3a5fd41ce834bc2cc
BeforeSunset16 <145817984+BeforeSunset16@users.noreply.github.com>
2023-10-08 01:14:05 -0400

    Update activities

--- src/app/activities/_component/ContentCard.tsx
+++ src/app/activities/_component/ContentCard.tsx
@@ -15,11 +15,15 @@ export default function ContentCard(props: React.PropsWithChildren<Record<string
             <div className={styles['card-image']}>
                 <FallbackImage src={imgSrc} alt="图片描述"/>
             </div>
-            <div className={styles['card-content']}>
+            <div className={styles['card-content']}>           
+                <div className={styles.tag}>
+                    <span>节日</span>
+                    <span>tag2</span>
+                </div>
                 <h2> {title} </h2>
                 <i> {date} </i>
                 <p> {content} </p>
-                <a className={styles.more} href={link}> More </a>
+                <a href={link}> 查看详情 </a>
             </div>
 
         </div>
